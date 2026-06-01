468f4cfdfec233c26ff2712d6474bdb94c176234
Anushka Thakur <anushka.thakur1701@gmail.com>
2026-06-01 21:54:24 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/1/2026, 9:54:24 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        unordered_map<char,int>counter;
+
+        for(char ch:s){
+            counter[ch]=counter[ch]+1;
+        }
+        
+        for(char ch:t){
+            if(counter.find(ch)==counter.end() || counter[ch]==0){
+                return false;
+            }
+            counter[ch]=counter[ch]-1;
+        }
+        return true;
+    }
+};
