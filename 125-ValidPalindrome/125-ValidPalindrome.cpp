e200f9127827815fd57c8aebcc915c77639f12e2
Anushka Thakur <anushka.thakur1701@gmail.com>
2026-06-01 21:54:27 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,28 @@
+// Last updated: 6/1/2026, 9:54:28 PM
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+
+        while (start < end) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+
+            if (tolower(s[start]) != tolower(s[end]))
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
