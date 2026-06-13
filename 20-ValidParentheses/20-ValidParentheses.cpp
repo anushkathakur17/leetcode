a444e66a8f2e6e3fc900f7a5c7e39a72335d3f32
Anushka Thakur <anushka.thakur1701@gmail.com>
2026-06-13 13:57:10 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,29 @@
+// Last updated: 6/13/2026, 1:57:10 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(c==')' && st.top()!='('){
+                    return false;
+                }
+                if(c=='}' && st.top()!='{'){
+                    return false;
+                }
+                if(c==']' && st.top()!='['){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
